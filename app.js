37345f1e689472757dede682a512d257d63a2e05
Hana <133090931+Rubyde@users.noreply.github.com>
2024-03-05 18:57:17 +0800

    update slides properties

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ window.Webflow ||= [];
 window.Webflow.push(() => {
   $(".slider-main_component").each(function (index) {
     const swiper = new Swiper($(this).find(".swiper")[0], {
-      slidesPerView: "1",
+      slidesPerView: 1,
       speed: 700,
       keyboard: true,
       mousewheel: {
         forceToAxis: true,
       },
+      slideToClickedSlide: true,
+      followFinger: false,
     });
   });
 });
