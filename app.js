d386580e4e050b2d7617334865b1707fe42b6807
Hana <133090931+Rubyde@users.noreply.github.com>
2024-03-05 18:49:55 +0800

    slidesPerview added

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 window.Webflow ||= [];
 window.Webflow.push(() => {
   $(".slider-main_component").each(function (index) {
-    const swiper = new Swiper($(this).find(".swiper")[0], {});
+    const swiper = new Swiper($(this).find(".swiper")[0], {
+      slidesPerView: "1",
+      speed: 700,
+      keyboard: true,
+      mousewheel: true,
+    });
   });
 });
